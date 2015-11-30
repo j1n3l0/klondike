35a2969303998b57a4391481acab7bfb17adc72a
Nelo Onyiah <nelo.onyiah@gmail.com>
2015-11-30 21:11:12 +0000

    In the process of checking last card is upturned

--- src/klondike/core.clj
+++ src/klondike/core.clj
@@ -3,12 +3,14 @@
 (defn new-board
   ""
   []
-  (let [deck (new-deck)]
-    {:piles (for [n (range 1 8)] (take n (take 28 deck)))
+  (let [deck (take 28 (shuffle (new-deck)))]
+    {:piles (for [n (range 1 8)] (take n deck))
      :foundations {:clubs [] :diamonds [] :hearts [] :spades []}
      :deck (drop 28 deck)}))
 
 (defn new-deck
   ""
   []
-  (for [suit #{:clubs :diamonds :hearts :spades} value #{"ace" 2 3 4 5 6 7 8 9 10 "jack" "queen" "king"}] {:suit suit :value value}))
+  (for [suit #{:clubs :diamonds :hearts :spades}
+        value #{"ace" 2 3 4 5 6 7 8 9 10 "jack" "queen" "king"}]
+    {:suit suit :value value :upturned? false}))
--- test/klondike/core_test.clj
+++ test/klondike/core_test.clj
@@ -15,6 +15,14 @@
     (is (= (range 1 8)
            (map count (:piles (new-board)))))))
 
+(deftest last-card-in-each-pile-upturned
+  (testing "the last card in each pile should be upturned"
+    (is (= true (every? :upturned? (map last (:piles (new-board))))))))
+
+#_(deftest all-but-the-last-card-is-not-upturned
+  (testing "all but the last card in each pile is not upturned"
+    (is (apply = false ()))))
+
 (deftest test-new-deck
-  (testing "a new deck of 52 cards"
+  (testing "a new deck should contain 52 cards"
     (is (= (count (new-deck)) 52))))
